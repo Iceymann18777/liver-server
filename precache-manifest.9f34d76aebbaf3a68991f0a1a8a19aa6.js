e4c23db42b6c365e1e326694614ce1989713ff0a
adam khader <82147328+Iceymann18777@users.noreply.github.com>
2021-08-23 21:39:11 +1000

    Initialize gh-pages

---
+++ precache-manifest.9f34d76aebbaf3a68991f0a1a8a19aa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf590aa3289853d6f6718b28b4d00398",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4798eae901d467f6672",
+    "url": "./static/css/2.12427dd7.chunk.css"
+  },
+  {
+    "revision": "864cd7e780015158106e",
+    "url": "./static/css/main.811e8d3d.chunk.css"
+  },
+  {
+    "revision": "c4798eae901d467f6672",
+    "url": "./static/js/2.91daede2.chunk.js"
+  },
+  {
+    "revision": "576d770f1aeb9e10228723362e9e052b",
+    "url": "./static/js/2.91daede2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "864cd7e780015158106e",
+    "url": "./static/js/main.e3c621fd.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
